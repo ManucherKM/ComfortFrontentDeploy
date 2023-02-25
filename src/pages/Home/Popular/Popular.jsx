fcf0b54400324eea580da74dba467573ca91a9d3
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2023-02-25 16:10:41 +0500

    Update Popular.jsx

--- src/pages/Home/Popular/Popular.jsx
+++ src/pages/Home/Popular/Popular.jsx
@@ -11,12 +11,13 @@ const Popular = () => {
     fetch.then(res => setPopular(res.data.flights))
 
     return (
-        <div className='container mt-24'>
+        <div />
+        {/*<div className='container mt-24'>
             <h2 className='text-3xl font-medium text-center mb-12'>Популярное</h2>
             <div className='flex justify-between items-center md:flex-col dek:gap-8'>
                 {populars !== null && populars.length !== 0 && populars.map((popular) => <Card pathUrl={`/reservation/${popular.id}`} key={popular.id} imgUrl={popular.imgUrl} title={popular.title} subtitle={popular.subtitle} hiddenId={popular.id} />)}
             </div>
-        </div>
+        </div>*/}
     )
 }
 
